6f6c0f0dc218a1b7e0d8301b06741829d23a2c05
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2024-10-31 18:35:30 -0300

    controller structure

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("users")]
+        public async Task<ActionResult<List<User>>> getUsers()
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    ID = "xxxxxxxxxxxxx",
+                    Name = "x",
+                    Email = "x@x.com",
+                    Password = "xxxxx@"
+                }
+
+            };
+
+            return Ok(users);
+        }
+        //[HttpGet("users/{id}")]
+        //[HttpPost("users/login")]
+        //[HttpPost("users")]
+        //[HttpPut("users{id}")]
+        //[HttpDelete("users{id}")]
+    }
+}
