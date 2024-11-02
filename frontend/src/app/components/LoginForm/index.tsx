12c3ddf44ac48d14574c762fadf7496965c929a4
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2024-11-01 23:02:43 -0300

    create login logic and auth logic

--- frontend/src/app/components/LoginForm/index.tsx
+++ frontend/src/app/components/LoginForm/index.tsx
@@ -1,29 +1,67 @@
 import { FormEvent } from "react";
+import { useRouter } from 'next/navigation'
 
 export default function LoginForm() {
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
 
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const user = {
+        "id": "x",
+        "name": "x",
+        "email": formData.get('email')?.toString(),
+        "password": formData.get('password')?.toString(),
+        "isActive": false
+    }
+
+    if (user.email == null || user.password == null || user.email.length == 0 || user.password.length == 0) {
+      alert('Invalid credentials.');
+      return;
+    } else if (user.email.length > 100) {
+      alert('E-mail too long, 100 max characters.');
+      return;
+    } else if (user.password.length > 100) {
+      alert('Password too long, 100 max characters.');
+      return;
+    } else if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[\S]{8,}$/.test(user.password)) {
+      alert('Password must contain at least one special character, one number, one upper case and one lower case.');
+      return;
+    }
+
     try {
-      const response = await fetch('http://localhost:8090/api/users', {
-        method: 'GET',
+      const response = await fetch('http://localhost:8090/api/users/login', {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify(user)
       });
-
-      console.log(response);
   
-      if (!response.ok) {
+      if (response.status != 200) {
+        const contentType = response.headers.get("content-type");
+        const errorMessage = contentType && contentType.includes("application/json")
+          ? (await response.json()).message
+          : await response.text();
+    
+        alert(errorMessage);
         throw new Error(`HTTP error! status: ${response.status}`);
       }
   
       const data = await response.json();
-      console.log(data);
+      sessionStorage.setItem('userSession', JSON.stringify(data));
+
+      const storedSession = JSON.parse(sessionStorage.getItem('userSession') || '{}');
+
+      console.log(storedSession);
+
+      router.push('/home');
   
     } catch (error) {
-      console.error('Error fetching users:', error);
+      //console.error('Error fetching users:', error);
     }
   }
 
@@ -47,8 +85,9 @@ export default function LoginForm() {
             <div className="box-border flex items-center p-4 gap-2 
                 w-full h-[52px] 
                 bg-white border border-[#E8E9EA] rounded-[6px] 
-                focus-within:border-[#F72793]">
+                focus-within:border-[rgb(247,39,147)]">
               <input 
+                name="email"
                 type="email" 
                 placeholder="Introduce tu email" 
                 className=" font-medium flex-grow border-none outline-none focus:outline-none focus:border-transparent"
@@ -64,6 +103,7 @@ export default function LoginForm() {
                 bg-white border border-[#E8E9EA] rounded-[6px] 
                 focus-within:border-[#F72793]">
               <input 
+                name="password"
                 type="password" 
                 placeholder="Introduce tu contraseña" 
                 className=" font-medium flex-grow border-none outline-none focus:outline-none focus:border-transparent"
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,30 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation'
+import { User } from "@/types";
+
 export default function HomePage() {
+  const router = useRouter();
+  const [storedSession, setStoredSession] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const sessionData = sessionStorage.getItem("userSession");
+      if (sessionData) {
+        setStoredSession(JSON.parse(sessionData));
+      } else {
+        router.push('/');
+      }
+    }
+  }, []);
+
   return (
     <>
-
+      {storedSession ? (
+        <h1>Nice, {storedSession.email}</h1>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </>
-  )
+  );
 }
